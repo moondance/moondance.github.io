f55cd910ec3675097db0974e53fa58b179f7a099
moondance <yongp1985@gmail.com>
2025-12-01 20:27:59 +0800

    Site updated: 2025-12-01 20:27:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2025/apple-id/","posts/2025/about-blog/","posts/2025/hello-world/","posts/2025/cloudflare-speed-ip/","posts/2025/labubu-style-prompt/","posts/2025/netfix-gongchang/","posts/2025/pic-bed/"];function toRandomPost(){
+var posts=["posts/2025/apple-id/","posts/2025/cloudflare-speed-ip/","posts/2025/hello-world/","posts/2025/netfix-gongchang/","posts/2025/pic-bed/","posts/2025/jisu-english/","posts/2025/about-blog/","posts/2025/labubu-style-prompt/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
